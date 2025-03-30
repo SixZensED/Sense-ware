c4d63089e14e004625539bf922b6e33b1f95f76b
SixZens <106856251+SixZensED@users.noreply.github.com>
2025-03-31 03:16:02 +0700

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,1 +1,20 @@
+local id = {7074860883}
 
+local request = syn and syn.request or http and http.request or http_request or request or httprequest
+local access = nil
+for _, v in ipairs(id) do
+	if game.GameId == v then
+		access = v
+		break
+	end
+end
+
+if access then
+	local response = request({
+		Url = string.format("https://raw.githubusercontent.com/SixZensED/Sense-ware/refs/heads/main/list/%s.lua",access),
+		Method = "GET"
+	})
+	if response and response.Body then
+		loadstring(response.Body, true)()
+	end
+end
